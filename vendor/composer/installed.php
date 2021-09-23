b6b82aa5925647ff7cbca33645bb9699a3f7d469
arbjorck <arbjorck@gmail.com>
2021-09-23 16:48:02 +0200

    API composer require stripe

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '31b70b379ad58ce9eaae564004f13d2c2bc0fe3b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '31b70b379ad58ce9eaae564004f13d2c2bc0fe3b',
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v7.97.0',
+            'version' => '7.97.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'reference' => 'ae41c309ce113362706f8d5f19cf0cf2c730bc4a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
